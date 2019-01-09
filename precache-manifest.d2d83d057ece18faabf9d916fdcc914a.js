b5575fa0e1d2fc0010afea3066eee0e6de0ccd56
Douglas Julião Marques <douglas_juliao16@hotmail.com>
2019-01-09 08:58:53 -0200

    267b14f

---
+++ precache-manifest.d2d83d057ece18faabf9d916fdcc914a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "06c335252425bd7666df",
+    "url": "/assinatura-email-f360/css/app.1ca80083.css"
+  },
+  {
+    "revision": "06c335252425bd7666df",
+    "url": "/assinatura-email-f360/js/app.757cd2d3.js"
+  },
+  {
+    "revision": "e17f382f18ff4b798685",
+    "url": "/assinatura-email-f360/css/chunk-vendors.4509d561.css"
+  },
+  {
+    "revision": "e17f382f18ff4b798685",
+    "url": "/assinatura-email-f360/js/chunk-vendors.3c829d10.js"
+  },
+  {
+    "revision": "791b9c3406090ba89ac96b5410d76af4",
+    "url": "/assinatura-email-f360/img/ex1.791b9c34.gif"
+  },
+  {
+    "revision": "86fd322c34659a31b3d5e9c5a164ae80",
+    "url": "/assinatura-email-f360/img/ex2.86fd322c.gif"
+  },
+  {
+    "revision": "ec6450c3a4ec6c239fc0c8f268f69430",
+    "url": "/assinatura-email-f360/index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/assinatura-email-f360/robots.txt"
+  },
+  {
+    "revision": "2fc3f31cce1d4b860d6f982e696b34a9",
+    "url": "/assinatura-email-f360/sw.js"
+  }
+];
